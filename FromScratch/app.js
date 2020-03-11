dd4e175e9ae27ac50acd261590d538d7b9bd0e69
zipzapzup <kelvin.jumino@gmail.com>
2020-03-11 22:51:34 +1100

    JavaScript, implement search Tasks and Remove

--- FromScratch/app.js
+++ FromScratch/app.js
@@ -34,6 +34,16 @@ function loadEventListeners() {
     form.addEventListener('submit', addTask );
     // List of all DOM Event, AKA: Event Listener is in: 
     // https://www.w3schools.com/jsref/dom_obj_event.asp
+
+    // Add Remova task event. An event listener to the delete event. An event when part of the Task List class is clicked, it will delete it.
+    taskList.addEventListener('click', removeTask);
+
+    // Add a clear task event. An event when we click the button class will clear it.
+    clearBtn.addEventListener('click', clearTask);
+
+    // Add a filter event, when we filter up using the key up.
+    filter.addEventListener('keyup', filterTasks);
+
 }
 
 // Add task button, all of the function
@@ -51,7 +61,7 @@ function loadEventListeners() {
 function addTask(e) {
     if(taskInput.value === ''){
         alert('Add a task');
-    }
+    } else {
     // Part1
     // Create li element
     const li = document.createElement('li');
@@ -81,6 +91,44 @@ function addTask(e) {
     taskInput.value = '';
     console.log(li);
 
-
+    }
     e.preventDefault();
+}
+
+function removeTask(e){
+    if ( e.target.parentElement.classList.contains('delete-item') ){
+        if( confirm('Are you Sure you want to Delete?')){
+            e.target.parentElement.parentElement.remove();
+        }  
+    }
+
+}
+
+
+
+function clearTask(){
+    while(taskList.firstChild){
+        taskList.removeChild(taskList.firstChild);
+}
+}
+
+function filterTasks(e){
+    const text = e.target.value.toLowerCase();
+
+    // Note in here query Selector selects all of the collection-item class. Collection-item class is only the one that we created from the addTasks function. Selecting those, mean we select only the tasks here.
+
+    // Further if statement is used to display it. ForEach loop will go and apply the task function, which it will check the textcontent of the collection-item. 
+    
+    // If the item (to lower case) is in the index of the "text" that is typed. If no match or known as -1, 
+    document.querySelectorAll('.collection-item').forEach(function(task){
+        const item = task.firstChild.textContent;
+
+        if(item.toLowerCase().indexOf(text) != -1){
+            task.style.display = 'block';
+        } else {
+            task.style.display = 'none';
+
+        }
+    });
+
 }
