a1a276cc33a52a7c69cd8383cb7ba168d0c86e9f
zipzapzup <kelvin.jumino@gmail.com>
2020-03-19 02:08:45 +1100

    JavaScript Fetch API

--- FromScratch/Fetch API/app.js
+++ FromScratch/Fetch API/app.js
@@ -1,20 +1,61 @@
 // Re-creating HTTP API using Fetch API
 
 document.getElementById('button1').addEventListener('click', getText);
-document.getElementById('button2').addEventListener('click', getText);
-document.getElementById('button3').addEventListener('click', getText);
+document.getElementById('button2').addEventListener('click', getJSON);
+document.getElementById('button3').addEventListener('click', getExternalAPI);
 
-
+// Get local text file
 function getText() {
-    fetch('test1.txt')
+    fetch('test.txt')
         .then(function(res){
             return res.text();
 
         })
         .then(function(data){
             console.log(data);
+            document.getElementById('output').innerHTML = data;
         })
         .catch(function(err){
             console.log(err);
         });
+}
+
+// Get local JSON data
+function getJSON() {
+    fetch('posts.json')
+        .then(function(res){
+            return res.json();
+        })
+        .then(function(data){
+            console.log(data);
+            let output = '';
+            data.forEach(function(post){
+                output += `<li>${post.title}</li>`;
+            });
+            document.getElementById('output').innerHTML = output;
+
+        })
+        .catch(function(err){
+            console.log(err);
+        });
+}
+
+function getExternalAPI(){
+    fetch('https://api.github.com/users')
+        .then(function(res){
+            return res.json();
+        })
+        .then(function(data){
+            console.log(data);
+            let output = '';
+            data.forEach(function(user){
+                output += `<li>${user.login}</li>`;
+            });
+            document.getElementById('output').innerHTML = output;
+
+        })
+        .catch(function(err){
+            console.log(err);
+        });
+
 }
