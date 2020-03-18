2e61f27299d02c6d950fa899f78791da5e121864
zipzapzup <kelvin.jumino@gmail.com>
2020-03-18 22:33:05 +1100

    JavaScript EasyHTTP Library

--- FromScratch/Custom HTTP AJAX/app.js
+++ FromScratch/Custom HTTP AJAX/app.js
@@ -1,12 +1,58 @@
 const http = new easyHTTP;
 
 // Get Posts
-http.get('https://jsonplaceholder.typicode.com/posts', function(err, posts){
+// http.get('https://jsonplaceholder.typicode.com/posts', function(err, posts){
+//     if(err) {
+//         console.log(err);
+//     } else {
+//         console.log(posts);
+//     }
+
+// })
+
+// Get Single Post
+
+// http.get('https://jsonplaceholder.typicode.com/posts/1', function(err, post){
+//     if(err) {
+//         console.log(err);
+//     } else {
+//         console.log(post);
+//     }
+
+// })
+
+// POST Request
+// Create Data
+
+const data = {
+    title: 'Custom Post',
+    body: 'This is a custom post'
+};
+
+// // Create POST Request
+// http.post('https://jsonplaceholder.typicode.com/posts', data, function(err, post){
+//     if (err) {
+//         console.log(err);
+//     } else {
+//         console.log(post);
+//     }
+// });
+
+// Update Post (PUT updates it)
+
+// http.put('https://jsonplaceholder.typicode.com/posts/1', data, function(err, post){ 
+//     if(err) {
+//         console.log(err);
+//     } else {
+//         console.log(post);
+//     }
+// });
+
+http.delete('https://jsonplaceholder.typicode.com/posts/1', function(err, response){
     if(err) {
         console.log(err);
     } else {
-        console.log(posts);
+        console.log(response);
     }
 
 })
-
