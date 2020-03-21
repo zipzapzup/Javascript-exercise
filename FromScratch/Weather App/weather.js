64eb63216af686ade9b79ed2d82460a46a178ddf
zipzapzup <kelvin.jumino@gmail.com>
2020-03-21 18:14:40 +1100

    Weather App

---
+++ FromScratch/Weather App/weather.js
@@ -0,0 +1,24 @@
+class Weather {
+    constructor(city, state) {
+        this.apiKey = '0b1b1cd6fccb8a40d3a81ba818cdeb31';
+        this.city = city;
+        this.state = state;
+    }
+
+    // Fetch weather from API
+    async getWeather() {
+        const response = await fetch(`http://api.openweathermap.org/data/2.5/forecast?q=${this.city}&appid=${this.apiKey}`);
+
+        const responseData = await response.json();
+
+        return responseData.list
+
+    }
+
+
+    // Change weather location
+    changeLocation(city, state) {
+        this.city = city;
+        this.state = state;
+    }
+}
