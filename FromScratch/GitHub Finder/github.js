b1dbaa0bfa6e3e70b48bf2673dc3ee615d30a469
zipzapzup <kelvin.jumino@gmail.com>
2020-03-21 12:03:22 +1100

    GitHub FInder

--- FromScratch/GitHub Finder/github.js
+++ FromScratch/GitHub Finder/github.js
@@ -2,9 +2,11 @@ class GitHub {
     constructor() {
         this.client_id = 'f0094a6b40b067708f81';
         this.client_secret = 'f3394ee3628910cd259632f82619c8b9f71ae3b0';
-        this.token = 'e523996e147310b0355e7fe22a823b25d25b3611';
+        this.token = '';
         this.user = 'zipzapzup'
         this.authorisation_header = btoa(`${this.user}:${this.token}`)
+        this.repos_count = 5;
+        this.repos_sort = 'created: asc';
 
     }
 
@@ -12,16 +14,23 @@ class GitHub {
         const profileResponse = await fetch(`https://api.github.com/users/${user}`,{
             headers: {
                 'Content-type': 'application/json',
-                'Authorization' :`basic ${this.authorisation_header}` 
-           
+                'Authorization' :`basic ${this.authorisation_header}`   
         }
+        });
 
-
+        const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?sort=${this.repos_sort}`,{
+            headers:{
+                'Content-type': 'application/json',
+                'Authorization' :`basic ${this.authorisation_header}`   
+            }
         });
 
         const profile = await profileResponse.json();
+        const repos = await repoResponse.json();
+
         return {
-            profile: profile
+            profile: profile,
+            repos
 
         }
     }
