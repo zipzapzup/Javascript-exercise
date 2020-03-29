c70316ebe05eac1801a9b99812b4f709c35e4b7e
Kelvin <juminok@au1.ibm.com>
2020-03-29 18:51:49 +1100

    JavaScript - Edit Meal Calories

--- FromScratch/Large Project - MealTracker/app.js
+++ FromScratch/Large Project - MealTracker/app.js
@@ -108,6 +108,7 @@ const ItemCtrl = (function() {
 const UICtrl = (function() {
     const UISelectors = {
         itemList: '#item-list',
+        listItems: '#item-list li',
         addBtn: ".add-btn",
         updateBtn: ".update-btn",
         deleteBtn: ".delete-btn",
@@ -153,6 +154,25 @@ const UICtrl = (function() {
             </a>`;
             // Insert item
             document.querySelector(UISelectors.itemList).insertAdjacentElement('beforeend', li)
+
+        },
+        updateListItem: function(item){
+            let listItems = document.querySelectorAll(UISelectors.listItems);
+            
+            // Turn Node list into array
+            listItems = Array.from(listItems);
+            listItems.forEach(function(listItem){
+                const itemID = listItem.getAttribute('id');
+
+                if(itemID === `item-${item.id}`){
+                    document.querySelector(`#${itemID}`).innerHTML = `<strong>${item.name} </strong>  <em>${item.calories} Calories</em>
+                    <a href="#" class="secondary-content">
+                        <i class="edit-item fa fa-pencil"></i>
+                    </a>`;
+
+                }
+
+            });
 
         },
         clearInput: function(){
@@ -285,7 +305,15 @@ const App = (function(ItemCtrl, UICtrl) {
         // Update item
         const updatedItem = ItemCtrl.updateItem(input.name, input.calories);
 
+        // Update UI
+        UICtrl.updateListItem(updatedItem);
 
+        // Get total calories
+        const totalCalories = ItemCtrl.getTotalCalories();
+        // Add total calories to UI
+        UICtrl.showTotalCalories(totalCalories);
+        
+        UICtrl.clearEditState();
         e.preventDefault();
     }
 
