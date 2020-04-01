1f43ee458654041723169a10b93042ed4eccc589
zipzapzup <kelvin.jumino@gmail.com>
2020-04-02 00:02:29 +1100

    JavaScript - MicroProject completed

--- FromScratch/MicroPost_NodeJS/src/app.js
+++ FromScratch/MicroPost_NodeJS/src/app.js
@@ -7,6 +7,17 @@ document.addEventListener('DOMContentLoaded', getPosts);
 // Listen for add post
 document.querySelector('.post-submit').addEventListener('click', submitPost);
 
+// Listen for delete
+// document.querySelector('#posts').addEventListener('click');
+
+// Listen for edit-state
+document.querySelector('#posts').addEventListener('click', enableEdit);
+
+// Listen for cancel
+document.querySelector('.card-form').addEventListener('click', cancelEdit)
+
+
+
 // Get Post
 function getPosts() {
   http.get('http://localhost:3000/posts')
@@ -19,19 +30,69 @@ function getPosts() {
 function submitPost() {
   const title = document.querySelector('#title').value;
   const body = document.querySelector('#body').value;
-
+  const id = document.querySelector('#id').value;
   const data = {
     title: title,
     body: body
   }
 
-  // Create POST method
-  http.post('http://localhost:3000/posts', data)
-    .then( data => {
-      ui.showAlert('Post added', 'alert alert-success');
-      ui.clearFields();
-      getPosts();
-    })
-    .catch( err => console.log(err));
 
+  if(title === '' || body === '') {
+    ui.showAlert('Please fill in all fields','alert alert-danger');
+
+  }  else {
+    if(id === '') {
+      // Create POST method
+      http.post('http://localhost:3000/posts', data)
+       .then( data => {
+          ui.showAlert('Post added', 'alert alert-success');
+          ui.clearFields();
+          getPosts();
+         })
+       .catch( err => console.log(err));
+
+    } else {
+      // Update Post
+      http.put(`http://localhost:3000/posts/${id}`, data)
+      .then( data => {
+         ui.showAlert('Post updated', 'alert alert-success');
+         ui.changeFormState('add');
+         getPosts();
+        })
+      .catch( err => console.log(err));
+
+    }
+
+
+}
+}
+
+function enableEdit(e) {
+  if(e.target.parentElement.classList.contains('edit')) {
+    const id  = e.target.parentElement.dataset.id;
+    const title = e.target.parentElement.previousElementSibling.previousElementSibling.textContent;
+    const body = e.target.parentElement.previousElementSibling.textContent;
+
+
+    const data = {
+      id: id,
+      title: title,
+      body: body
+    }
+
+    ui.fillForm(data);
+
+  }
+  console.log(e.target);
+
+  e.preventDefault();
+}
+
+//  Cancel edit state
+function cancelEdit(e) {
+  if(e.target.classList.contains('post-cancel')) {
+    ui.changeFormState('add');
+  }
+  
+  e.preventDefault();
 }
--- FromScratch/MicroPost_NodeJS/src/ui.js
+++ FromScratch/MicroPost_NodeJS/src/ui.js
@@ -4,7 +4,7 @@ class UI {
         this.titleInput = document.querySelector('#title');
         this.bodyInput = document.querySelector('#body');
         this.idInput = document.querySelector('#id');
-        this.postSubmit = document.querySelector('#post-submit');
+        this.postSubmit = document.querySelector('.post-submit');
         this.forState = 'add';
     }
 
@@ -53,7 +53,7 @@ class UI {
         }, 5000);
 
     }
-
+    // Clear alerts
     clearAlert() {
         const currentAlert = document.querySelector('.alert');
         
@@ -61,10 +61,56 @@ class UI {
             currentAlert.remove();
         }
     }
-
+    // Clear all fields
     clearFields() {
         this.bodyInput.value = '';
         this.titleInput.value = '';
+    }
+
+    // Fill form to edit
+    fillForm(data) {
+        this.titleInput.value = data.title;
+        this.bodyInput.value = data.body;
+        this.idInput.value = data.id;
+
+        this.changeFormState('edit');
+    }
+
+    clearIdInput() {
+        this.idInput.value = '';
+    }
+
+    changeFormState(type) {
+        if(type === 'edit') {
+            this.postSubmit.textContent = 'Update Post';
+            this.postSubmit.className = 'post-submit btn btn-warning btn-block';
+
+            // Create cancel button
+            const button = document.createElement('button');
+            button.className = 'post-cancel btn btn0light btn-block';
+            button.appendChild(document.createTextNode('Cancel Edit'));
+
+            // Get parent
+            const cardForm = document.querySelector('.card-form');
+            // Get element to insert before
+            const formEnd = document.querySelector('.form-end');
+            // Insert cancel button
+            cardForm.insertBefore(button, formEnd);
+
+        } else {
+            this.postSubmit.textContent = 'Post it';
+            this.postSubmit.className = 'post-submit btn btn-primary btn-block';
+            // Remove cancel btn if it is there
+
+            if(document.querySelector('.post-cancel')) {
+                document.querySelector('.post-cancel').remove();
+            }
+            // Clear ID from hidden field
+            this.clearIdInput();
+            // Clear text
+            this.clearFields();
+        }
+
     }
 }
 
