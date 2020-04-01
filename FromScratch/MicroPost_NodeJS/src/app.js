58aba63848ca3007d00cb4100826920544aa63d0
zipzapzup <kelvin.jumino@gmail.com>
2020-04-01 21:50:17 +1100

    JavaScript - MicroPosts adding a POST method

--- FromScratch/MicroPost_NodeJS/src/app.js
+++ FromScratch/MicroPost_NodeJS/src/app.js
@@ -1,12 +1,35 @@
 import { http } from './http.js';
 import { ui } from './ui.js';
+
 // Get posts on DOM load
 document.addEventListener('DOMContentLoaded', getPosts);
 
+// Listen for add post
+document.querySelector('.post-submit').addEventListener('click', submitPost);
 
+// Get Post
 function getPosts() {
   http.get('http://localhost:3000/posts')
     .then(data => ui.showPosts(data))
     .catch(err => console.log(err));
+
+}
+
+// Add Post
+function submitPost() {
+  const title = document.querySelector('#title').value;
+  const body = document.querySelector('#body').value;
+
+  const data = {
+    title,
+    body
+  }
+
+  // Create POST method
+  http.post('http://localhost:3000/posts', data)
+    .then( data => {
+      getPosts();
+    })
+    .catch( err => console.log(err));
 
 }
